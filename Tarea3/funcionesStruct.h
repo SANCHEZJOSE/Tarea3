6f6fb02635b142faeb915320b029975079914539
xornet93 <44046901+xornet93@users.noreply.github.com>
2018-12-15 17:09:48 -0300

    Add files via upload

--- Tarea3/funcionesStruct.h
+++ Tarea3/funcionesStruct.h
@@ -1,15 +1,15 @@
 #ifndef FUNCIONES_STRUCT_H
 #define FUNCIONES_STRUCT_H
-#define N 126
+#define tam 126
 #define nodos 8
 
 struct Protocolo{
 	int cmd;//3 bits --> 7 comandos
 	int Long;//7 bits --> 127 bits maximo
 	int ttl;//4 bits -->max 15 nodos
-	unsigned char data[N];//126 --> 126 caracteres maximo
+	unsigned char data[tam];//126 --> 126 caracteres maximo
 	int fcs;//10 bits -->1023 bits
-	unsigned char frame[N+3];//130
+	unsigned char frame[tam+3];//130
 };
 struct Ethernet{
 	unsigned char MACD[6];
@@ -24,8 +24,7 @@ struct Matrices{
 	int mac[nodos][6];//mac de cada nodos
 	char nombres[nodos][10];//nombre de los nodos
 	Matrices(){
-		for(int i=0;i<nodos;i++){
-			for(int j=0;j<4;j++){ttl[i][j]=-1;}
-		}}
+		for(int i=0;i<nodos;i++){for(int j=0;j<4;j++){ttl[i][j]=-1;}}
+	}
 };
 #endif
